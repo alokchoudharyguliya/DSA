893408f4acbeb3ef6547ef919a6b59c2aec792dd
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-03 12:19:58 +0530

    BST Started

---
+++ BST/CeilInBinaryTree.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+// What is CEIL ? Ceiling of a value refers to the value of the smallest node in the binary search tree that is greater than or equal to the given key.
+
+struct Node{
+	int data;
+	Node*left;
+	Node*right;
+	Node(int val):data(val),left(nullptr),right(nullptr){}
+};
+int main(){
+	
+}
