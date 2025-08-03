ecf483f3ae1c3ef4b55ccbfee5de55f713c9a96e
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-03 14:18:53 +0530

    Added

--- DeleteFromBST.cpp
+++ DeleteFromBST.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// Find where the node to deleted appears ? 
 // Deleting can be done in two ways for a non-leaf node:
 // 1.	Accept the left subtree as the new root which will take place of the node deleted. Join the right subtree to the right most node of the current subtree.
 // 2.	Accept the right subtree as new root which will take place of the node deleted. Join the left subtree to the left most node of the current subtree.
