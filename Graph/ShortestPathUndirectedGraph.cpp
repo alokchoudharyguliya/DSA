473a21fdfd5a33ec8bc5b5cbcb7a67f5d4e963c0
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-08 16:18:07 +0530

    Added

--- Graph/ShortestPathUndirectedGraph.cpp
+++ Graph/ShortestPathUndirectedGraph.cpp
@@ -1,5 +1,11 @@
-#include<bits/stdc++.h.>
+#include<bits/stdc++.h>
 using namespace std;
+class Solution{
+	public:
+		vector<int>shortestPath(vector<vector<int>>&edges,int V,int E,int src){
+
+		}
+};
 int main(){
 	int V=9, E=10;
 	vector<vector<int>>edges={{0,1},{0,3},{3,4},{4,5},{5,6},{1,2},{2,6},{6,7},{7,8},{6,8}};
