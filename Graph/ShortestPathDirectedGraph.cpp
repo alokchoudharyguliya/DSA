c512d3c285c5cf97fd81ab3e3e9846b0dfd388c5
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-08 17:32:56 +0530

    Added

--- Graph/ShortestPathDirectedGraph.cpp
+++ Graph/ShortestPathDirectedGraph.cpp
@@ -13,6 +13,35 @@ class Solution{
 				int wt=edges[i][2];
 				adj[u].push_back({v,wt});
 			}
+			int vis[E]={0};
+			stack<int>st;
+			for(int i=0;i<E;i++){
+				if(!vis[i])
+					topoSort(i,adj,vis,st);
+
+			}
+			vector<int>dist[E];
+			for(int i=0;i<E;i++){
+				dist[i]=1e9;
+			}
+			dist[0]=0;
+			while(!st.empty()){
+				int node=st.top();
+				st.pop();
+				for(auto it:adj[node]){
+					int v=it.first;
+					int wt=it.second;
+
+					if(dist[node]+wt<dist[v]){
+						dist[v]=dist[node]+wt;
+					}
+
+				}
+
+			}
+			for(int i=0;i<E;i++)
+				if(dist[i]==1e9)dist[i]=-1;
+			return dist;
 		}
 };
 int main(){
