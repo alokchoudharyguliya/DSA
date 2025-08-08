5602709ab5f65dff4820e150d6df18b1dc2d3f16
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-08 16:46:21 +0530

    Added

--- Graph/ShortestPathDirectedGraph.cpp
+++ Graph/ShortestPathDirectedGraph.cpp
@@ -3,5 +3,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+	int V=6,E=7;
+	vector<vector<int>>edges={{0,1,2},{0,4,1},{4,5,4},{4,2,2},{1,2,3},{2,3,6},{5,3,1}};
+	Solution sol;
+	vector<int>ans=sol.shortestPath(V,E,edges);
 
 }
