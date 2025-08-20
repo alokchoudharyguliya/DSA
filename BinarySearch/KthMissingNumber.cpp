9b0af6af11ce90d8b3b8642d72e00356e9419df4
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-20 11:22:08 +0530

    Roman to Integer added

---
+++ BinarySearch/KthMissingNumber.cpp
@@ -0,0 +1,31 @@
+// arr[]={2,3,4,7,11} k=5
+// 1,5,6,8,9,10 missing 
+// 5th number is 9
+
+// for the present numbers, the number that is present results ki humara missing number ek aage shift ho jayega
+// for the current number, the current number will take one place
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[]={2,3,4,7,11};
+    int k=5;
+    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++)
+    {
+        if(arr[i]<k)
+            k++;
+        else
+        break;
+    }
+    cout<<k;
+}
+
+// What about its optimization?
+// Current time - O(N) space - O(1)
+// Optimized version lesser than O(N) will be O(logN) also given array is sorted so binary search not nomral but modified
+
+// Why we cannot use typical binary search ?
+
+// because we are looking for the number it is not present in the array
+// What about binary search on answers
+//
---
+++ Strings/Atoi.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Atoi(string s){
+    if(s.empty())return 0;
+
+    const long long MAX_INT=INT_MAX;
+    const long long MIN_INT=INT_MIN;
+
+    int n=s.length();
+    while(i!=n){
+
+    }
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<Atoi(s);
+}
