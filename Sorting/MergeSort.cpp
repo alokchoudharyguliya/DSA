1a0c2a652b4441c68a47b2d1258b01bdbf283b2d
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-20 23:57:16 +0530

    Merge Sort added

---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>mergeSort(vector<int>&arr,int start,int end){
+    int n=end-start;
+    int mid=end+start/2;
+    
+    return arr;
+}
+int main(){
+        vector<int>arr={1,2,7,5,4,1,6,3,9};
+        arr=mergeSort(arr);
+        for(int i=0;i<arr.size();i++){
+            cout<<arr[i];
+        }
+}
