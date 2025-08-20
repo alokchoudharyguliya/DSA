c02121589e079b5944405c88b8c42888d5129211
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-20 11:27:09 +0530

    Roman to Integer added

--- Strings/Atoi.cpp
+++ Strings/Atoi.cpp
@@ -2,14 +2,35 @@
 using namespace std;
 int Atoi(string s){
     if(s.empty())return 0;
-
-    const long long MAX_INT=INT_MAX;
-    const long long MIN_INT=INT_MIN;
-
     int n=s.length();
-    while(i!=n){
-
+    int i=0;
+    while(i<n && s[i]==' '){
+        i++;
     }
+    if(i==n)return 0;
+    int sign=1;
+    if(s[i]=='-')
+        {
+            sign=-1;
+            i++;
+        }
+        else if(s[i]=='+')
+        {
+            i++;
+        }
+        long long res=0;
+        long long ans=0;
+        while(i<n&&isdigit(s[i])){
+            int digit=s[i]-'0';
+            res=res*10+digit;
+            ans=sign*res;
+            if(ans<=INT_MIN)
+            return INT_MIN;
+            if(ans>=INT_MAX)
+            return INT_MAX;
+            i++;
+        }
+        return static_cast<int>(ans);
 }
 int main(){
     string s;
