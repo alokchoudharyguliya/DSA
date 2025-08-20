89b23a0069c33e4328a29a1155403f7722dbee6c
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-20 23:49:48 +0530

    Insertion Sort added

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 vector<int>insertion(vector<int>&arr){
     int n=arr.size();
-    for(int i=0;i<n-1;i++){
+    for(int i=1;i<n;i++){
         int temp=arr[i];
-        int j=i;
-        while(j>=0){
-            if(temp<arr[j])
-                    arr[j+1]=arr[j];
-            j--;
-        }
-        arr[j]=temp;
+        int j=i-1;
+        while(j>0 && temp<arr[j])
+             {       arr[j+1]=arr[j];
+                    j--;
+            }
+        arr[j+1]=temp;
         // cout<<temp;
         
     }
