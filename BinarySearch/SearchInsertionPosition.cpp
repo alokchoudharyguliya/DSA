71fc3307c61c573fedb4107f38c5c29d9fb5dc31
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-22 22:55:52 +0530

    SearchInsertionPosition added

--- BinarySearch/SearchInsertionPosition.cpp
+++ BinarySearch/SearchInsertionPosition.cpp
@@ -1,10 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	vector<int>arr={1,2,3,1,5,4,6,8,7,11};
+	vector<int>arr={1,2,3,3,8,9,11};
+	int tar=8;
 	int low=0;
+	int ans=0;
+	int target=10;
 	int high=arr.size()-1;
 	while(low<=high){
-		
+		int mid=(low+high)/2;
+		if(arr[mid]<target){
+			low=mid+1;
+		}
+		else{
+			ans=mid;
+			high=mid-1;
+		}
 	}
+	cout<<ans<<"\n";
 }
