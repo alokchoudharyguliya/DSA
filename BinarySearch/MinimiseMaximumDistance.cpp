bde41426cdabc07eec7b265f22436388ffd05226
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-23 21:40:17 +0530

    Added

---
+++ BinarySearch/MinimiseMaximumDistance.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>arr={1,2,3,4,5};
+    int k=4;
+    long double 
+    for(int gas=1;i<=k;i++)
+    {
+        maximumVal=-1, maxInd=-1nt 
+    }
+}
