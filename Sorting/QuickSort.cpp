e67ca8c35e223d3a034fe712111e813528ace6ca
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-24 18:20:03 +0530

    Quick Sort added

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,13 @@
+// a divide and conquer algorithm like Merge Sort
+// unlike merge sort does not use any extra array for sorting
+// uses an auxiliary space .: slightly better than merge sort
+
+// Algo is basically a repetition of two simple steps:
+// 1. Pick a pivot and place it in its correct place in the sorted array
+// 2. Shift smaller elements (i.e. smaller than pivot) on the left of the pivot and larger ones to the right.
+// Choosing the pivot
+// It can be any element of our choice
+// first element of the array
+// last element of the array
+// median of the array
+// any random element of the array
