c10310a9909dc2365e55370d1613b24fa736ff1f
Pseudophoenix <waynerooney0089@gmail.com>
2025-09-06 01:25:34 +0530

    DisjointSet added

--- Graph/DisjointSet.cpp
+++ Graph/DisjointSet.cpp
@@ -1,23 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Disjoint{
-    vector<int>rank, parent; // -> rank considers the height only and not the number of nodes
-    public:
-    Disjoint(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
+class Disjoint
+{
+    vector<int> rank, parent; // -> rank considers the height only and not the number of nodes
+public:
+    Disjoint(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
         }
     }
-    int findUPar(int node){
-        if(parent[node]==node)return node;
-        else return parent[node]=findUPar(parent[node]); // this compresses the disjoint set data structure
+    int findUPar(int node)
+    {
+        if (parent[node] == node)
+            return node;
+        else
+            return parent[node] = findUPar(parent[node]); // this compresses the disjoint set data structure
     }
-    int unionByRank(int u,int v){
-        
+    int unionByRank(int u, int v)
+    {
+        int u_par = findUPar(u);
+        int v_par = findUPar(v);
+        if (rank[u_par] < rank[v_par])
+        {
+            parent[u_par] = v_par;
+        }
+        else if (rank[u_par] >= rank[v_par])
+        {
+            parent[v_par] = u_par;
+            rank[u_par]++;
+        }
     }
 };
-int main(){
-
+int main()
+{
 }
