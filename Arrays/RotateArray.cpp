4b1f997e379753cabd1bbaa5ebf1c30d0a80987b
Pseudophoenix <waynerooney0089@gmail.com>
2025-09-10 23:38:57 +0530

    Added

--- Arrays/RotateArray.cpp
+++ Arrays/RotateArray.cpp
@@ -2,14 +2,35 @@
 // 1. Using a temp array
 // 2. Observation based 
 // reverse the last k elements, reverse the n-k first elements, then reverse the whole array
+
 #include<bits/stdc++.h>
 using namespace std;
+
+void Reverse(int arr[],int start,int end){
+    // using 2 pointers and swap 
+    // start at left side and end from right side of array
+    while(start<=end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+
 void RotateRight(int arr[],int n,int k){
-    if(n==0)return;
-    k=k%n;
-    if(k>n)return ;
+    // reverse the first n-k elements
+    Reverse(arr,0,n-k-1);
+    // reverse last k elements
+    Reverse(arr,n-k,n-1);
+    // reverse the whole array
+    Reverse(arr,0,n-1);
 
-    int temp[k];
-    for(int i=n-k;i<n;i++)temp[i-n+k]=arr[i];
-
+}
+int main(){
+    int arr[]={1,2,3,4,5,6,7,7};
+    int n=8;
+    int k=2;
+    RotateRight(arr,n,k);
+    printArray(arr);
 }
