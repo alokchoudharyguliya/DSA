513aae5c0e5b6f99e1f2f3279f2cfd8b7ab19db9
Pseudophoenix <waynerooney0089@gmail.com>
2025-09-11 13:35:48 +0530

    Added

---
+++ BitManipulation/PowerSet.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define fio                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+void pV(vector<int> arr)
+{
+    for (auto i : arr)
+        cout << i << " ";
+    cout << endl;
+}
+int main()
+{
+    fio;
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+
+    // }
+    int n=3;
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> ans;
+    int subsets = 1 << n;
+    for (int num = 0; num < subsets; num++)
+    {
+        vector<int> lists;
+        for (int i = 0; i <= n - 1; i++)
+        {
+            if (num & (1 << i))
+                lists.push_back(nums[i]);
+        }
+        ans.push_back(lists);
+    }
+    for(auto it:ans)
+    pV(it);
+    return 0;
+}
+
+// TC -> N*2^n
