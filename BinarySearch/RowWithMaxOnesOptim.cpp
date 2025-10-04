2129770b938a3c92905e1f36bb106f17eaede84b
Pseudophoenix <waynerooney0089@gmail.com>
2025-10-04 20:02:00 +0530

    Added

---
+++ BinarySearch/RowWithMaxOnesOptim.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int rowWithMax1s(vector<vector<int>> mat, int n, int m)
+{
+    int cnt_max = 0;
+    int ind = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        int cnt_ones = 0;
+        for (int j = 0; j < m; j++)
+        {
+            cnt_ones += mat[i][j];
+        }
+        if (cnt_max < cnt_ones)
+        {
+            cnt_max=cnt_ones;
+            ind=i;
+        }
+    }
+    return ind;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 1, 1}, {0, 0, 1}, {0, 0, 0}};
+    int n = 3, m = 3;
+    cout << rowWithMax1s(matrix, n, m);
+}
