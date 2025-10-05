1f85e75b0789017f8c16f61200d072bdfb6d0853
Pseudophoenix <waynerooney0089@gmail.com>
2025-10-05 16:33:05 +0530

    Subsequence With Sum K added

---
+++ Recursion/SubsequenceWithSumK.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int helper(vector<int>&arr,int sum,int ind){
+    if(sum==0){
+        return 1;}
+    if(ind==arr.size()||sum<0){
+        return 0;
+    }
+    return helper(arr,sum-arr[ind],ind+1)+helper(arr,sum,ind+1);
+}
+int solve(vector<int>&arr,int k){
+    return helper(arr,k,0);
+}
+int main(){
+    int k=10;
+    vector<int>arr={4,9,2,5,1};
+    cout<<solve(arr,k);
+}
