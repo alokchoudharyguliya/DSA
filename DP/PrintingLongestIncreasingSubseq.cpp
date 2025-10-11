d5ae9e85d107e08da644c7b10fa4a55cb5620073
Pseudophoenix <waynerooney0089@gmail.com>
2025-10-11 16:21:32 +0530

    Printing LIS added

--- DP/PrintingLongestIncreasingSubseq.cpp
+++ DP/PrintingLongestIncreasingSubseq.cpp
@@ -2,7 +2,44 @@
 using namespace std;
 int longestIncreasingSubseq(int arr[], int n)
 {
- 
+    vector<int> dp(n, 1);
+    vector<int> hash(n, 1);
+    for (int i = 0; i <= n - 1; i++)
+    {
+        hash[i] = i;
+        for (int prev_index = 0; prev_index <= i - 1; prev_index++)
+        {
+            if (arr[prev_index] < arr[i] && 1 + dp[prev_index] > dp[i])
+            {
+                dp[i] = 1 + dp[prev_index];
+                hash[i] = prev_index;
+            }
+        }
+    }
+    int ans = -1;
+    int lastIndex = -1;
+    for (int i = 0; i <= n - 1; i++)
+    {
+        if (dp[i] > ans)
+        {
+            ans = dp[i];
+            lastIndex = i;
+        }
+    }
+    vector<int> temp;
+    temp.push_back(arr[lastIndex]);
+    while (hash[lastIndex] != lastIndex)
+    {
+        lastIndex = hash[lastIndex];
+        temp.push_back(arr[lastIndex]);
+    }
+    reverse(temp.begin(), temp.end());
+    for (int i = 0; i < temp.size(); i++)
+    {
+        cout << temp[i] << ",";
+    }
+    cout << endl;
+    return ans;
 }
 int main()
 {
