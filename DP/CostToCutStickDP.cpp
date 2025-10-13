61dbf7407061beae5892950639ef8c51ddae4188
Pseudophoenix <waynerooney0089@gmail.com>
2025-10-13 23:38:44 +0530

    MCM Cut Stick Tabulation added

--- DP/CostToCutStickDP.cpp
+++ DP/CostToCutStickDP.cpp
@@ -1,28 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findMinimumCost(int i, int j, vector<int> &cuts, vector<vector<int>> &dp)
-{
-    if (i > j)
-        return 0;
-    if (dp[i][j] != -1)
-        return dp[i][j];
+// int findMinimumCost(int i, int j, vector<int> &cuts, vector<vector<int>> &dp)
+// {
+//     if (i > j)
+//         return 0;
+//     if (dp[i][j] != -1)
+//         return dp[i][j];
 
-    int mini = INT_MAX;
-    for (int ind = i; ind <= j; ind++)
-    {
-        int ans = cuts[j + 1] - cuts[i - 1] + findMinimumCost(i, ind - 1, cuts, dp) + findMinimumCost(ind + 1, j, cuts, dp);
-        mini = min(mini, ans);
-    }
-    return dp[i][j] = mini;
-}
+//     int mini = INT_MAX;
+//     for (int ind = i; ind <= j; ind++)
+//     {
+//         int ans = cuts[j + 1] - cuts[i - 1] + findMinimumCost(i, ind - 1, cuts, dp) + findMinimumCost(ind + 1, j, cuts, dp);
+//         mini = min(mini, ans);
+//     }
+//     return dp[i][j] = mini;
+// }
+// int minimumCost(int n, int c, vector<int> &cuts)
+// {
+//     cuts.push_back(n);
+//     cuts.push_back(0);
+//     sort(cuts.begin(), cuts.end());
+//     vector<vector<int>> dp(c + 1, vector<int>(c + 1, -1));
+//     return findMinimumCost(1, c, cuts, dp);
+// }
 int minimumCost(int n, int c, vector<int> &cuts)
 {
     cuts.push_back(n);
     cuts.push_back(0);
     sort(cuts.begin(), cuts.end());
-    vector<vector<int>> dp(c + 1, vector<int>(c + 1, -1));
-    return findMinimumCost(1, c, cuts, dp);
+    vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
+    for (int i = c; i >= 1; i--)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (i > j)
+                continue;
+            int mini = INT_MAX;
+            for (int ind = i; ind <= j; ind++)
+            {
+                int ans = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                mini = min(mini, ans);
+            }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][c];
 }
 int main()
 {
