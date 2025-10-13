813651997c40d70ed06bbef875973447bbf69679
Pseudophoenix <waynerooney0089@gmail.com>
2025-10-13 22:28:10 +0530

    MCM DP added

--- DP/MatrixChain.cpp
+++ DP/MatrixChain.cpp
@@ -17,7 +17,6 @@ int matrixMultiplication(vector<int> &arr, int n)
 {
     int i = 1;
     int j = n - 1;
-    vector<int>dp();
     return f(arr, i, j);
 }
 
@@ -27,3 +26,38 @@ int main()
     int n = arr.size();
     cout << matrixMultiplication(arr, n);
 }
+
+
+// DP Memoized
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int f(vector<int> &arr, int i, int j, vector<vector<int>> &dp)
+// {
+//     if (i == j)
+//         return 0;
+//     if (dp[i][j] != -1)
+//         return dp[i][j];
+//     int mini = INT_MAX;
+//     for (int k = i; k <= j - 1; k++)
+//     {
+//         int ans = f(arr, i, k, dp) + f(arr, k + 1, j, dp) + arr[i - 1] * arr[k] * arr[j];
+//         mini = min(mini, ans);
+//     }
+//     return dp[i][j] = mini;
+// }
+
+// int matrixMultiplication(vector<int> &arr, int n)
+// {
+//     int i = 1;
+//     int j = n - 1;
+//     vector<vector<int>> dp(n, vector<int>(n, -1));
+//     return f(arr, i, j, dp);
+// }
+
+// int main()
+// {
+//     vector<int> arr = {10, 20, 30, 40, 50};
+//     int n = arr.size();
+//     cout << matrixMultiplication(arr, n);
+// }
