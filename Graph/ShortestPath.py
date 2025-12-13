927ffde12a5aaa0325a49f3a2d7c3fdffed39f35
alokchoudharyguliya <waynerooney0089@gmail.com>
2025-12-13 22:30:04 +0530

    Added Shortest Path

---
+++ Graph/ShortestPath.py
@@ -0,0 +1,33 @@
+from collections import deque, defaultdict
+
+class Solution:
+    def shortestPath(self,edges,N,M,src):
+        adj=defaultdict(list)
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        dist=[float('inf')]*N
+        dist[src]=0
+        q=deque()
+        q.append(src)
+        while q:
+            node=q.popleft()
+            for neighbor in adj[node]:
+                if dist[node]+1<dist[neighbor]:
+                    dist[neighbor]=dist[node]+1
+                    q.append(neighbor)
+
+        for i in range(N):
+            if dist[i]==float('inf'):
+                dist[i]=-1
+        return dist
+            
+
+N=9
+M=10
+edges=[
+    [0,1],[0,3],[3,4],[4,5],[5,6],[1,2],[2,6],[6,7],[7,8],[6,8]
+]
+obj=Solution()
+res=obj.shortestPath(edges,N,M,0)
+print(" ".join(map(str,res)))
